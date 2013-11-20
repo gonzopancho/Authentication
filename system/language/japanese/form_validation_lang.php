7f2ffa1cc8be93f88ffcc40db6d8ec795c273a03
tuntunkun <naoya@tuntunkun.com>
2013-11-20 13:35:32 +0900

    add system

---
+++ system/language/japanese/form_validation_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['required'] 			= "%s を入力してください。";
+$lang['isset']				= "%s は空欄にできません。";
+$lang['valid_email']		= "%s には正しいEmailアドレスを入力する必要があります。";
+$lang['valid_emails'] 		= "%s には正しいEmailアドレスを入力する必要があります。";
+$lang['valid_url'] 			= "%s には正しいURLを入力する必要があります。";
+$lang['valid_ip'] 			= "%s には正しいIPアドレスを入力する必要があります。";
+$lang['min_length']			= "%s は最低 %s 文字以上でなければなりません。";
+$lang['max_length']			= "%s は %s 文字を超えてはいけません。";
+$lang['exact_length']		= "%s は %s 文字でなければなりません。";
+$lang['alpha']				= "%s 欄には、半角アルファベット以外は入力できません。";
+$lang['alpha_numeric']		= "%s には、半角英数字以外は入力できません。";
+$lang['alpha_dash']			= "%s には、半角英数字、アンダースコア(_)、ハイフン(-)以外は入力できません。";
+$lang['numeric']			= "%s には、数字以外は入力できません。";
+$lang['is_numeric']			= "%s には、数値以外は入力できません。";
+$lang['integer']			= "%s には、整数以外は入力できません。";
+$lang['regex_match']		= "%s 欄は、正しい形式ではありません。";
+$lang['matches']			= "%s が %s 欄と一致しません。";
+$lang['is_natural']			= "%s には、正の整数以外は入力できません。";
+$lang['is_natural_no_zero']	= "%s 欄には、0より大きい整数以外は入力できません。";
+$lang['decimal']			= "%s は10進数しか入力できません。";
+$lang['less_than']			= "%s は %s より小さい数しか入力できません。";
+$lang['greater_than']		= "%s は %s より大きな数しか入力できません。";
+$lang['form_validation_alpha_numeric']	= '{field} には、英数字のみ入力できます。';
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/japanese/form_validation_lang.php */
